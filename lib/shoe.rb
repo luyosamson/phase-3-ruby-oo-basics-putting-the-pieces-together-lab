b61eb4286a1b314593eb1a615d45ca4c75c02afa
luyosamson <luyosamson@gmail.com>
2022-10-21 00:55:47 +0300

    Assignment completed

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,26 @@
 # Make your shoe class here!
 class Shoe
-    attr_accessor:color, :size, :material, :condition
+    
     attr_reader :brand
+    attr_reader :material
+    attr_reader :size
+    attr_reader :color
+    attr_reader :condition
     def initialize(brand)
         @brand=brand
 
+    end
+    def color=(color)
+        @color=color
+    end
+      def material=(material)
+        @material=material
+    end
+      def size=(your_size)
+        @size=your_size
+    end
+      def condition=(status)
+        @condition=status
     end
 
     def cobble
